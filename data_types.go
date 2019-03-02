22a239e25a8027fbcbd3553c138dda48f281571b
Justin <drakefjustin@gmail.com>
2019-03-02 19:38:54 +0100

    Update data_types.go
    Golf out 1 line (type Seconds). Notice that `Timestamp` is denominated in seconds so we could use `Seconds` everywhere.

--- data_types.go
+++ data_types.go
@@ -1,20 +1,20 @@
 package beacon_challenge
 
+// uint64
 type Slot uint64
 type Epoch uint64
 type Shard uint64
-type ValidatorIndex uint64
 type Gwei uint64
+type Timestamp uint64
+type ValidatorIndex uint64
+type DepositIndex uint64
+type BlsDomain uint64 // TODO: Rename to BLSDomain
+
+// byte arrays
 type Root [32]byte
 type Bytes32 [32]byte
 type BLSPubkey [48]byte
 type BLSSignature [96]byte
-type BlsDomain uint64
-
-type DepositIndex uint64
-
-type Timestamp uint64
-type Seconds uint64
 
 type ValueFunction func(index ValidatorIndex) Gwei
 
