4257ee114d2cf70ab677be85161c793194631215
protolambda <proto@protolambda.com>
2019-03-02 21:11:16 +0100

    line counter

--- README.md
+++ README.md
@@ -17,6 +17,18 @@ Challenge accepted.
 
 Git ref of beacon spec used for this challenge: [8df7de150e68408b78d6488a6f4c9cf8e18c4405](https://github.com/ethereum/eth2.0-specs/blob/8df7de150e68408b78d6488a6f4c9cf8e18c4405/specs/core/0_beacon-chain.md) (Current dev branch at the time of starting the challenge).
 
+## Counting lines
+
+```
+bash linecount.bash
+```
+
+Yes this is bad. But fun, and code golfing = more scrutiny towards spec, we look more at the necessity of functions.
+ I.e. the spec improves in the process.
+
+In the future this codebase aims to provide an "executable spec": the specced state transition should be runnable,
+ and the compiling process already helps enforce consistency in the spec (and typing of Go helps as well)
+
 ## License
 
 MIT. Container types are adapted from CC0-licensed types in original ETH 2.0 specification doc.
---
+++ linecount.bash
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+gofilesStr=$(find ./ -name '*.go' |tr "\n" " ")
+
+gofiles=($gofilesStr)
+
+# sort it
+IFS=$'\n' gofiles=($(sort <<<"${gofiles[*]}"))
+unset IFS
+
+printf "file                        brackets         comments        blanks        not-counting        counting         full\n"
+
+TOTAL_COUNT=0
+for i in "${gofiles[@]}"
+do
+    if  [ "$i" = "./bls_util.go" ]; then
+        continue
+    fi
+    BRACKETS=`cat "$i" | grep -P "^\s*(\{|\}|\(|\))\s*$" | wc -l`
+    COMMENTS=`cat "$i" | grep -P "\s*//.+$" | wc -l`
+    BLANKS=`cat "$i" | grep -P "^$" | wc -l`
+    FULL=`cat "$i" | wc -l`
+    NOT_COUNTING=$(($BRACKETS + $COMMENTS + $BLANKS))
+    COUNTING=$(($FULL - $NOT_COUNTING))
+    TOTAL_COUNT=$(($TOTAL_COUNT + $COUNTING))
+    printf "$i\t                    $BRACKETS\t         $COMMENTS\t         $BLANKS\t         $NOT_COUNTING\t         $COUNTING\t         $FULL\n"
+done
+
+echo "total counting lines: $TOTAL_COUNT"
